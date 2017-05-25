414c09c139797b646db78ef4e13f550b8d47fcb4
manglass <egman24@gmail.com>
2017-05-25 10:12:09 -0400

    cleans up functional patterns

--- EPOparser.py
+++ EPOparser.py
@@ -1,75 +1,76 @@
 from lxml import etree
-
 import os
-
 import zipfile
-
 import datetime
+import functools
 
-# os.getcwd()
-# 
+def fmap(fn, coll):
+  return functools.reduce(lambda acc, val: acc + [fn(val)], coll, [])
 
-# dir(etree)
+def ffilter(predicateFn, coll):
+	return functools.reduce(lambda acc, val: (acc + [val]) if predicateFn(val) else acc, coll, [])
 
-# list( map(lambda x: x.attrib, tree.xpath('//abstract')) )[0]
-# list( map(lambda x: x.text, tree.xpath('//abstract')) )[0]
+def fcmap(fn):
+  return lambda coll: functools.reduce(lambda acc, val: acc + [fn(val)], coll, [])
 
-# no webservice to just pull xml programatically, need the filesystem dirs and unzips?
-# if needed, which part/parts should be threaded? the traversal? first get all paths, then spawn unzip(parse) for each
+def fcfilter(predicateFn):
+  return lambda coll: functools.reduce(lambda acc, val: (acc + [val]) if predicateFn(val) else acc, coll, [])
 
-###
-### traverse (walk through to each dir, run function when inside -- use os commands)
-###
+def fzip(coll1, coll2):
+	return list(zip(coll1, coll2))
 
-###
-### unzip (parse run as callback when complete)
-###
+def fcompose(*functions):
+    return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)  
 
-###
-### parse (takes 'intellegence' data structure argument which provides necessary Elements and fields (can be changed and passed in at later dates as necessary) -> single dir piece of main state)
-###
-
-# assumes the right file in current context passed into tree...
-
-def getFileInfo(indexFilename):
-	return list(zip(map(lambda element: element.text, list(tree(indexFilename).xpath('//filename'))), map(lambda element: element.text, list(tree(indexFilename).xpath('//file-location')))))
-
-def tree(filename):
-	return etree.parse(filename, etree.XMLParser(encoding='utf-8', recover=True))
-
-# get :: -> [Element]
-def get(tree, elementPath):
-	return tree.xpath(elementPath)
-
-# getAll :: [{}], '' -> ?
-def getAll(tree, element, info, alias):
-	return list(map(lambda el: ( alias, el.tag, getattr(el, info) ), get(tree, element)))
-
-# def get(Element, info): return Element[0][info] # works with standard dict, not sure what the one comming from tree is
-
-# getAll :: Tree, [[ field, extraction, alias ]] -> [{}] 
-def parse(tree, fields):
-	return list(filter(lambda x: bool(x), list(map(lambda pair: getAll(tree, pair[0], pair[1], pair[2]), fields))))
-
-def config(): 
-	return [['//abstract', 'attrib', 'abstract'], ['//abstract', 'text', 'abstract'], ['//ep-patent-document', 'attrib', 'ep-patent-document'], ['//B001EP', 'text', 'nametocallit']]
+###########
+# helpers # 	
+###########
 
 def marker(id):
 	return id + '-parsecomplete'
 
-###
-### supervisor (keep state via inital -- returns from parse, munge them together as necessary for final, how to handle failures)
-###
+# fromElement :: attrib | text -> Element -> {} | String
+def fromElement(info):
+  return lambda el: getattr(el, info)
 
-# can parallelize/thread?
+# tree :: String -> Tree
+def tree(filename):
+	return etree.parse(filename, etree.XMLParser(encoding='utf-8', recover=True))
 
-def process(config, xmlFile, archiveFile, callback):
+# getFileInfo :: String -> [(filename, file-location)]
+def getFileInfo(indexFilename):
+	return fzip(
+    fmap(fromElement('text'), getElement(tree(indexFilename), '//filename')),
+    fmap(fromElement('text'), getElement(tree(indexFilename), '//file-location'))
+  )
+
+# getElement :: Tree, xpath -> [Element]
+def getElement(tree, elementPath):
+	return tree.xpath(elementPath)
+
+# getMetadata :: Tree, xpath, attrib | text, String -> [] | [(alias, tag, {} | String)]
+def getMetadata(tree, elementPath, info, alias):
+	return fmap(lambda el: ( alias, el.tag, getattr(el, info) ), getElement(tree, elementPath))
+
+#TODO: compose for longer chains
+def parse(tree, fields):
+  return fcompose(
+    fcmap(lambda x: x[0]),
+    fcfilter(lambda x: bool(x)),
+    fcmap(lambda field: getMetadata(tree, field[0], field[1], field[2]))
+  )(fields)
+
+##############
+# processing #
+##############
+
+def process(fields, state, callback, xmlFile, archiveFile):
   if not os.path.isfile(marker(xmlFile)):
     zipfile.ZipFile(archiveFile, "r").extract(xmlFile)
     
     # add validation/testing
     
-    metadata = parse( tree(xmlFile), config() )
+    metadata = parse( tree(xmlFile), fields() )
     
     # send off to queue (handle failures and restarts), if success, write file...
     callback(metadata)
@@ -78,11 +79,13 @@ def process(config, xmlFile, archiveFile, callback):
     completefile = open(marker(xmlFile),"w") 
     completefile.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    # state[file[0]] = 'info!'
+    # gives us a final built object for logging/reporting, 
+    # may not want if it is too large in memory!
+    state[archiveFile] = metadata
     
     os.remove(xmlFile)  
 
-def traverse(file, config, state, callback):
+def traverse(file, fields, state, callback):
   initialDirectory = os.getcwd()
   workDirectory = file[1].replace('\\', '/')[1:]
 
@@ -94,18 +97,33 @@ def traverse(file, config, state, callback):
  
   os.chdir(workDirectory)
   #print(os.getcwd())
-  process(config, xmlFile, archiveFile, callback)
+  process(fields, state, callback, xmlFile, archiveFile)
   os.chdir(initialDirectory) 
 
-def run(path, config, state, callbackEach, callbackAll):
+def fields(): 
+	return [['//abstract', 'attrib', 'abstract'], ['//abstract', 'text', 'abstract'], ['//ep-patent-document', 'attrib', 'ep-patent-document'], ['//B001EP', 'text', 'nametocallit']]
+
+def run(path, fields, state, callbackEach, callbackAll):
   fileInfo = getFileInfo(path)
   for file in fileInfo:
-    traverse(file, config, state, callbackEach)
+    traverse(file, fields, state, callbackEach)
   callbackAll(state)
 
+#################
+# use and notes #
+#################  
+
 # import parser
-# EPOparser.run('index.xml', EPOparser.config, {}, lambda x: x, lambda x: x)
-# EPOparser.run('index.xml', EPOparser.config, {}, toQueue, print)
+
+# EPOParser.run('index.xml', EPOParser.fields, {}, lambda x: x, lambda x: x)
+# EPOParser.run('index.xml', EPOParser.fields, {}, toQueue, print)
 
 # capture doc id globally and state of document (kind code)... provide with each payload
 
+# no webservice to just pull xml programatically, need the filesystem dirs and unzips?
+# if needed, which part/parts should be threaded? the traversal? first get all paths, then spawn unzip(parse) for each
+
+
+# parts that can be parallelized, threaded?
+# processing/callbacks that can be sent IN to change the behavior at the callsite
+# ...
